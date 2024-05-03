25c9c34cdc237f81a537eed56609ad88fded824e
alexandrecostapb <costa.alexandre@ifpb.edu.br>
2024-05-03 16:32:24 -0300

    Update MainActivity.java

--- app/src/main/java/com/example/testegit1/MainActivity.java
+++ app/src/main/java/com/example/testegit1/MainActivity.java
@@ -20,6 +20,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        System.out.println("Teste");
+        System.out.println("Teste 2");
     }
 }
